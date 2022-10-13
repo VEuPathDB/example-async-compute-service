33bcc4c1812f8eca96a4339af55d41f143937251
Elizabeth <foxcapade@gmail.com>
2022-10-13 15:18:13 -0400

    update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
   // repository:
 
   // Core lib
-  implementation("org.veupathdb.lib:jaxrs-container-core:6.9.1")
+  implementation("org.veupathdb.lib:jaxrs-container-core:6.10.0")
 
   // Jersey
   implementation("org.glassfish.jersey.core:jersey-server:3.0.8")
